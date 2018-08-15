a2fa8a579df17c8dae2e3345396e54994933e35e
janasanches <janasanches1@hotmail.com>
2018-08-15 17:14:32 -0300

    Add files via upload
    System.out.print - é entrada de dado
    numero= entrada.next.Int() - recebe o numero informado pelo usuário
    System.out.Println - duvida???
    System.out.Print- duvida??

---
+++ par_ou_impar.java
@@ -0,0 +1,43 @@
+		import java.util.Scanner;
+		
+	public class par_ou_impar{
+		
+		public static void main (String args[] ){
+			
+			int numero;
+			
+			Scanner entrada= new Scanner(System.in);
+			//Entrada de dado
+			System.out.print("Digite um numero:\n");//??? posso colocar print ou println???
+			 numero = entrada.nextInt();// recebo o numero informado pelo usuário
+			
+			//Processamento: inicio dos testes
+			if(numero% 2 == 0){// divide o  numero informado por 2, se o resto for 0, é numero par.
+			
+			System.out.println("o numero e par:\n!");//  posso colocar print??
+			}
+			
+			
+			else
+			{
+			
+			System.out.println("Numero Impar\n");// posso colocar print??
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	
+	
+		}
+		
+	}
+	
+	
